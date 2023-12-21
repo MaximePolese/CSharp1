de152638625ecd35ef282a2c3eb27a9df55c7338
maxime.polese <maxime.polese@le-campus-numerique.fr>
2023-12-21 09:53:27 +0100

    Création Unit test

---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+using HelloWorld;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    [TestSubject(typeof(Program))]
+    public class CalculatorTests
+    {
+
+        [TestMethod]
+        public void METHOD()
+        {
+            
+        }
+    }
+}
